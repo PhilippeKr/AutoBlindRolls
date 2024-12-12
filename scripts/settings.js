0bf38ba94ed16d0581b63fa6c6ed7496728e14a1
PhilippeKr <77798281+PhilippeKr@users.noreply.github.com>
2024-12-12 18:55:02 +0100

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -11,8 +11,115 @@ export const registerMainSettings = function() {
 }
 
 export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideDisguise", {
+    name: "Hide Disguise",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideIntrigue", {
+    name: "Hide Intrigue",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+
+  
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideEvaluate", {
+    name: "Hide Evaluate",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+
+  
+export const registerSettingsRQG = function(scope) {
   game.settings.register("auto-blind-rolls", "hideSenseChaos", {
     name: "Hide Sense Chaos",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideSenseAssassin", {
+    name: "Hide Sense Assassin",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+
+  
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideInsight", {
+    name: "Hide Insight",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideListen", {
+    name: "Hide Listen",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideScan", {
+    name: "Hide Scan",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideSearch", {
+    name: "Hide Search",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideTrack", {
+    name: "Hide Track",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+
+  
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideHide", {
+    name: "Hide Hide",
+    scope: scope,
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+export const registerSettingsRQG = function(scope) {
+  game.settings.register("auto-blind-rolls", "hideMoveQuietly", {
+    name: "Hide Move Quietly",
     scope: scope,
     config: true,
     default: false,
