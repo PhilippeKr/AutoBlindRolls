a6c7709e19cbbcaa291cf967d6a9907fe199a77b
PhilippeKr <77798281+PhilippeKr@users.noreply.github.com>
2024-12-12 18:58:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,18 @@ Hooks.on('ready', () => {
 function startRQG(){
   Hooks.on("preCreateChatMessage", async (rawMessage) => {
     if (rawMessage.flavor.includes("Check")){
+      if(game.settings.get("auto-blind-rolls", "hideDisguise") && rawMessage.flavor.includes("Disguise")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideIntrigue") && rawMessage.flavor.includes("Intrigue")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideEvaluate") && rawMessage.flavor.includes("Evaluate")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideSenseAssassin") && rawMessage.flavor.includes("Sense Assassin")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideSenseChaos") && rawMessage.flavor.includes("Sense Chaos")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideSenseInsight") && rawMessage.flavor.includes("Insight")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideListen") && rawMessage.flavor.includes("Listen")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideScan") && rawMessage.flavor.includes("Scan")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideSearch") && rawMessage.flavor.includes("Search")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideTrack") && rawMessage.flavor.includes("Track")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideHide") && rawMessage.flavor.includes("Hide")){rawMessage.applyRollMode("blindroll");}
+      if(game.settings.get("auto-blind-rolls", "hideMoveQuietly") && rawMessage.flavor.includes("Move Quietly")){rawMessage.applyRollMode("blindroll");}
     }
 })
 }
