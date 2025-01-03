fbb0f3c035353af000e38cc91787718f003ac977
PhilippeKr <77798281+PhilippeKr@users.noreply.github.com>
2025-01-03 20:13:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,16 +25,21 @@ function startRQG(){
       if(game.settings.get("auto-blind-rolls", "hideDisguise") && rawMessage.flavor.includes("Disguise")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideIntrigue") && rawMessage.flavor.includes("Intrigue")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideEvaluate") && rawMessage.flavor.includes("Evaluate")){rawMessage.applyRollMode("blindroll");}
+
       if(game.settings.get("auto-blind-rolls", "hideSenseAssassin") && rawMessage.flavor.includes("Sense Assassin")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideSenseChaos") && rawMessage.flavor.includes("Sense Chaos")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideConceal") && rawMessage.flavor.includes("Conceal")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideInsight") && rawMessage.flavor.includes("Insight")){rawMessage.applyRollMode("blindroll");}
+      
       if(game.settings.get("auto-blind-rolls", "hideListen") && rawMessage.flavor.includes("Listen")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideScan") && rawMessage.flavor.includes("Scan")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideSearch") && rawMessage.flavor.includes("Search")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideTrack") && rawMessage.flavor.includes("Track")){rawMessage.applyRollMode("blindroll");}
+
       if(game.settings.get("auto-blind-rolls", "hideHide") && rawMessage.flavor.includes("Hide")){rawMessage.applyRollMode("blindroll");}
       if(game.settings.get("auto-blind-rolls", "hideMoveQuietly") && rawMessage.flavor.includes("Move Quietly")){rawMessage.applyRollMode("blindroll");}
+
+      if(game.settings.get("auto-blind-rolls", "hideDragonRune") && rawMessage.flavor.includes("Dragon (condition)")){rawMessage.applyRollMode("blindroll");}
     }
 })
 }
